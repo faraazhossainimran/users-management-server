b971542069a579a2e58781578155adc8e1f48c51
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-10-13 03:24:23 +0600

    get and post method added

---
+++ __steps__.js
@@ -0,0 +1,8 @@
+/**
+ * 1. create a post api on the server side
+ * 2. send data via post
+ * 3. set fetch method inside the fetch options (second parameter)
+ * 4. options will have three things: method
+ * 5. don't forget to send data by JSON.Stringify in the body:
+ * 7. On the server side: don't forget to use express.json()
+*/
